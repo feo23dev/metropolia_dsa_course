b2f397facf4c40f21931b8dc83c0b0808b89e81a
efe okyar <f.efe.okyar@gmail.com>
2024-05-05 21:41:37 +0300

    finished implementing doubly linked list

--- lists.py
+++ lists.py
@@ -1,10 +1,147 @@
 class ListNode:
-    def __init__(self, data=None, next=None):
+    def __init__(self, data=None, next=None, prev=None):
         self.data = data
         self.next = next
+        self.prev = prev
 
     def __repr__(self):
         return f"<ListNode: {self.data}>"
+
+class DoublyLinkedList:
+    def __init__(self):
+        self._head = self._tail = None;
+        self._size = 0
+    
+    def __repr__(self):
+        current_node = self._head
+        values = ''
+        while current_node:
+            values += f', {current_node.data}'
+            current_node = current_node.next
+        plural = '' if self._size == 1 else 's'
+        return f'<DoublyLinkedList ({self._size} element{plural}): [{values.lstrip(", ")}]>'
+    
+    def __len__(self):
+        return self._size
+    
+
+    def insert(self,index,value):
+        if index < 0 or index > self._size:
+            raise ValueError("Index must be in bounds")
+        
+        current_node = self._head
+
+        if index == 0:
+            print("YES I AM RUNNING!")
+
+            new_node = ListNode(value)
+            self._head = new_node
+            new_node.next = current_node
+        elif index == self._size:
+            print("NOW! THIS SHOULD RUN !")
+            current_node = self._tail
+            new_node = ListNode(value)
+            current_node.next = new_node
+            new_node.prev = current_node
+            
+        else:
+            for _ in range(index):
+            
+                current_node = current_node.next
+
+            new_node = ListNode(value)
+        
+            prev_node = current_node.prev
+            prev_node.next = new_node
+            new_node.prev = prev_node
+            new_node.next = current_node
+            current_node.prev = new_node
+
+        self._size +=1
+        
+       
+
+
+
+
+
+        print(current_node)
+
+    def remove(self,index):
+        if index < 0 or index > self._size-1:
+            
+            raise(ValueError("Out of bounds!"))
+           
+        
+
+        current_node = self._head
+        if index == 0:
+            next_node = current_node.next
+            self._head = next_node
+            
+            self._size -= 1
+            return current_node
+
+        if index == self._size-1:
+            
+            current_node = self._tail
+            prev = current_node.prev
+            prev.next= None
+            self._tail = prev
+            self._size -= 1
+            return current_node
+
+        
+        for _ in range(index):
+
+            current_node= current_node.next
+            
+        
+        prev = current_node.prev
+        next = current_node.next
+        prev.next = next
+        next.prev = prev
+        self._size -= 1
+       
+        return current_node
+
+
+
+    def append(self,value):
+
+        newNode = ListNode(value,next=None,prev=self._tail)
+
+        if self._head is None:
+            self._head = self._tail = newNode
+        else:
+            self._tail.next = newNode
+            self._tail = newNode
+        self._size +=1
+            
+    def pop(self):
+
+        if not self._size:
+            return None
+        
+        node_to_remove = self._tail
+        previous_node = node_to_remove.prev
+       
+        if node_to_remove == self._head:
+            self._head = None    
+        else:
+            previous_node.next = None
+
+        self._tail = previous_node
+
+        self._size -= 1
+
+        value = node_to_remove.data
+        del(node_to_remove)
+        return value
+
+
+
+
 
 
 class SinglyLinkedList:
@@ -130,13 +267,15 @@ class SinglyLinkedList:
 
 
 
-sll = SinglyLinkedList()
-sll.append("5")
-sll.append("6")
-sll.append("7")
-sll.append("8")
-sll.remove(0)
+dll = DoublyLinkedList()
+dll.append(10)
+dll.append(20)
+dll.append(30)
+dll.append(40)
+dll.append(50)
+dll.remove(4)
 
-print(sll)
+
+print(dll)
     
    
